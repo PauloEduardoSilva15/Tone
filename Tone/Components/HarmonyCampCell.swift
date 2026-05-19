73cf5652dd451681e39a849594e87922dfd2ce83
paulo <pauloeduardob75@gmail.com>
2026-05-19 17:56:27 -0300

    harmony camp

--- Tone/Components/HarmonyCampCell.swift
+++ Tone/Components/HarmonyCampCell.swift
@@ -5,3 +5,36 @@
 //  Created by Paulo Eduardo Barbosa da Silva on 19/05/26.
 //
 
+import SwiftUI
+
+struct HarmonyCampCell: View {
+    let note: String
+    @Environment(\.horizontalSizeClass) var sizeClass
+    
+    var body: some View {
+        VStack(spacing: sizeClass == .regular ? 12 : 8){
+            Text("I")
+                .font(sizeClass == .regular ? .headline : .caption)
+                .bold()
+            Text(note)
+                .bold()
+        }
+        
+        .frame(
+            width: sizeClass == .regular ? 110 : 70,
+            height: sizeClass == .regular ? 110 : 70
+        )
+        .border(.colorSecundary, width: 4)
+        .cornerRadius(sizeClass == .regular ? 16 : 12)
+        
+        
+        
+        
+        
+        
+    }
+}
+
+#Preview {
+    HarmonyCampCell(note: "C")
+}
