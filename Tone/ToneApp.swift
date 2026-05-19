5a7b08cbe1524ac96cdf39b979abf63149217f81
paulo <pauloeduardob75@gmail.com>
2026-05-19 14:00:40 -0300

    Initial Commit

---
+++ Tone/ToneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneApp.swift
+//  Tone
+//
+//  Created by Paulo Eduardo Barbosa da Silva on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ToneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
