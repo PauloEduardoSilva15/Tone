689761699ab7565cadad6b7a5a18dbd9798a9cd5
Igor Carrasco <196232533+BGNS42@users.noreply.github.com>
2026-05-20 15:09:58 -0300

    Correção no comportamento do carrossel e tamanho dinâmico dos círculos

--- Tone/Components/ChoiceKeyCarroussel.swift
+++ Tone/Components/ChoiceKeyCarroussel.swift
@@ -1,58 +1,67 @@
+////
+////  ChoiceKeyCarroussel.swift
+////  Tone
+////
+////  Created by Paulo Eduardo Barbosa da Silva on 19/05/26.
+////
 //
-//  ChoiceKeyCarroussel.swift
-//  Tone
-//
-//  Created by Paulo Eduardo Barbosa da Silva on 19/05/26.
-//
-
 import SwiftUI
 
 struct ChoiceKeyCarroussel: View {
-    var body: some View {
-        SnappingCarousel()
-    }
-}
-    
-struct SnappingCarousel: View {
     let notes = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"]
     
     private let itemMultiplier = 1000
     
     @State private var scrolledID: Int?
-
+    
     var body: some View {
         GeometryReader { geometry in
             let screenWidth = geometry.size.width
             
             // Definição dos tamanhos dos círculos
-            let selectedWidth: CGFloat = 65
-            let unselectedWidth: CGFloat = 40
-            let spacing: CGFloat = 10
-            
+            let selectedWidth: CGFloat = 85
+            let spacing: CGFloat = 16
+
             // O cálculo mágico: posiciona o elemento selecionado exatamente no centro
             // e dita quanto espaço sobra nas laterais para exibir os vizinhos
             let lateralPadding = (screenWidth - selectedWidth) / 2
             
             ScrollView(.horizontal, showsIndicators: false) {
-                LazyHStack(spacing: spacing) {
+                LazyHStack(alignment: .center, spacing: spacing) {
                     ForEach(0..<notes.count * itemMultiplier, id: \.self) { globalIndex in
                         let realIndex = globalIndex % notes.count
                         let note = notes[realIndex]
                         
-                        let isSelected = globalIndex == scrolledID
+                        // Descobre a distância entre o selecionado e os demais círculos
+                        let currentSelectedID = scrolledID ?? 0
+                        let distance = abs(globalIndex - currentSelectedID)
                         
+                        // Define as distâncias: se for 0 ele está selecionado. Se for 1, é um dos vizinhos próximos
+                        let isSelected = distance == 0
+                        let isNeighboor = distance == 1
+                        
+                        // definição dos tamanhos
+                        let size: CGFloat = isSelected ? 80 : (isNeighboor ? 55 : 35)
+                        let opacictyCircle: Double = isSelected ? 1 : (isNeighboor ? 0.8 : 0.5)
+                        
+                        let circleColor: Color = isSelected ? .colorPrimary : (isNeighboor ? .colorSecondary : .colorSecondary.opacity(opacictyCircle))
+                        let textColor: Color = isSelected ? .white : .black
+                        let textFont: Font = isSelected ? .title2 : (isNeighboor ? .body : .caption)
+                                                
                         Circle()
-                            .fill(isSelected ? .colorPrimary : .colorSecondary.opacity(0.5))
-                            .frame(width: isSelected ? selectedWidth : unselectedWidth, height: isSelected ? selectedWidth : unselectedWidth)
+                            .fill(circleColor)
+                            .frame(width: size, height: size)
                             .overlay(
                                 Text("\(note)")
-                                    .foregroundStyle(isSelected ? .white : .black)
-                                    .font(isSelected ? .title2 : .callout)
+                                    .foregroundStyle(textColor)
+                                    .font(textFont)
                                     .fontWeight(.semibold)
                             )
-                            .opacity(isSelected ? 1 : 0.4)
+                            .opacity(opacictyCircle)
+                            .id(globalIndex)
                             .animation(.interactiveSpring(response: 0.3, dampingFraction: 0.7), value: isSelected)
-                            .containerRelativeFrame(.horizontal) // Aligns to screen width
+                            //.animation(.interactiveSpring(response: 0.5, dampingFraction: 0.9), value: isNeighboor)
+                            .containerRelativeFrame(.horizontal, alignment: .center) // Aligns to screen width
                     }
                 }
                 .scrollTargetLayout() // Required for view-aligned snapping
@@ -61,16 +70,16 @@ struct SnappingCarousel: View {
             .scrollPosition(id: $scrolledID)
             .safeAreaPadding(.horizontal, lateralPadding)
         }
-        .frame(height: 80)
+        .frame(height: 120)
         .onAppear { // Faz o carrossel ficar infinito, pois cuida do reset do index do array notes
             let middleFactor = itemMultiplier / 2
             let initialIndex = middleFactor * notes.count
             scrolledID = initialIndex
         }
-        .padding(.vertical, 10)
     }
 }
 
 #Preview {
     ChoiceKeyCarroussel()
+        .padding(20)
 }
