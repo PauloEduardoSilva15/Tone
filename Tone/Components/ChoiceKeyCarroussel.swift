dbb5a798605d5e1720ab76924eebbaac4b23ca2d
Igor Carrasco <196232533+BGNS42@users.noreply.github.com>
2026-05-20 14:00:41 -0300

    Atualizado tamanho dos círculos

--- Tone/Components/ChoiceKeyCarroussel.swift
+++ Tone/Components/ChoiceKeyCarroussel.swift
@@ -20,6 +20,10 @@ struct SnappingCarousel: View {
     private let itemMultiplier = 1000
     
     @State private var scrolledID: Int?
+    
+//    private var selectedColor: Color {
+//        switch status
+//    }
 
     var body: some View {
         GeometryReader { geometry in
@@ -27,7 +31,8 @@ struct SnappingCarousel: View {
             
             // Definição dos tamanhos dos círculos
             let selectedWidth: CGFloat = 85
-            let unselectedWidth: CGFloat = 50
+            //let unselectedWidth: CGFloat = 50
+            //let defaultWidth: CGFloat = 30
             let spacing: CGFloat = 1
             
             // O cálculo mágico: posiciona o elemento selecionado exatamente no centro
@@ -39,19 +44,36 @@ struct SnappingCarousel: View {
                     ForEach(0..<notes.count * itemMultiplier, id: \.self) { globalIndex in
                         let realIndex = globalIndex % notes.count
                         let note = notes[realIndex]
+//                        let isSelected = globalIndex == scrolledID
                         
-                        let isSelected = globalIndex == scrolledID
+                        // Descobre a distância entre o selecionado e os demais círculos
+                        let currentSelectedID = scrolledID ?? 0
+                        let distance = abs(globalIndex - currentSelectedID)
+                        
+                        // Define as distâncias: se for 0 ele está selecionado. Se for 1, é um dos vizinhos próximos
+                        let isSelected = distance == 0
+                        let isNeighboor = distance == 1
+                        
+                        // definição dos tamanhos
+                        let size: CGFloat = isSelected ? 85 : (isNeighboor ? 60 : 35)
+                        let opacictyCircle: Double = isSelected ? 1 : (isNeighboor ? 0.5 : 0.3)
+                        
+                        let circleColor: Color = isSelected ? .colorPrimary : (isNeighboor ? .colorSecundary : .colorSecundary.opacity(opacictyCircle))
+                        let textColor: Color = isSelected ? .white : .black
+                        let textFont: Font = isSelected ? .title2 : (isNeighboor ? .body : .caption)
                         
                         Circle()
-                            .fill(isSelected ? .colorPrimary : .colorSecundary.opacity(0.5))
-                            .frame(width: isSelected ? selectedWidth : unselectedWidth, height: isSelected ? selectedWidth : unselectedWidth)
+                            .fill(circleColor)
+                        //.fill(isSelected ? .colorPrimary : .colorSecundary.opacity(0.5))
+                            .frame(width: size, height: size)
+                        //.frame(width: isSelected ? selectedWidth : unselectedWidth, height: isSelected ? selectedWidth : unselectedWidth)
                             .overlay(
                                 Text("\(note)")
-                                    .foregroundStyle(isSelected ? .white : .black)
-                                    .font(isSelected ? .title2 : .callout)
+                                    .foregroundStyle(textColor)
+                                    .font(textFont)
                                     .fontWeight(.semibold)
                             )
-                            .opacity(isSelected ? 1 : 0.4)
+                            .opacity(opacictyCircle)
                             .animation(.interactiveSpring(response: 0.3, dampingFraction: 0.7), value: isSelected)
                             .containerRelativeFrame(.horizontal) // Aligns to screen width
                     }
